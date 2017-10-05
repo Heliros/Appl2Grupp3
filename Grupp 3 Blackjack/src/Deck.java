e81a094cdb4a9d8fb57a58a08534497c41f2aa78
Jooona91 <32507717+Jooona91@users.noreply.github.com>
2017-10-05 12:33:17 +0200

    createDeck finished

--- Grupp 3 Blackjack/src/Deck.java
+++ Grupp 3 Blackjack/src/Deck.java
@@ -1,4 +1,56 @@
 
 public class Deck {
 
+	// Instance variables
+	int nrOfCards = 52;
+	
+	// Constructors
+	
+	
+	
+	// Class methods
+	String[] createDeck(String[] array){
+		
+		String[] color = {" of Hearts"," of Diamonds"," of Cloves"," of Spades"};
+		
+		for (int i = 0; i < color.length; i++) {
+			for (int j = 0; j < 13; j++) {
+				if (j < 9) {
+					array[j+(i*13)] = (j+2) + color[i];
+				} else {
+					switch (j) {
+					case 9: array[j+(i*13)] = "Jack" + color[i];
+						
+						break;
+					case 10: array[j+(i*13)] = "Queen" + color[i];
+						
+						break;
+					case 11: array[j+(i*13)] = "King" + color[i];
+						
+						break;
+					case 12: array[j+(i*13)] = "Ace" + color[i];
+						
+						break;
+
+					default:
+						break;
+					}
+				}
+			}
+		}
+		return array;
+	}
+	
+	int drawCard(){
+		
+		int cardValue = 0;
+		
+		
+		return cardValue;
+	}
+	
+	
+	// Getters and setters
+	
+	
 }
